7883dbb1e05026ce60f5a7e3d664a37e8658521f
Gaurangsharma <gaurang.sharma_cs16@gla.ac.in>
2020-09-07 20:16:08 +0530

    represent 2nd formate

---
+++ bargraph.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+# from selenium import webdriver
+
+
+# def get_value(url):
+#     driver = webdriver.Chrome()
+#     driver.get(url)
+#     driver.find_element_by_id('bt_gerar_cpf').click()
+#     while driver.find_element_by_id('texto_cpf').text == 'Gerando...':
+#         continue
+
+link=requests.get("https://www.allrecipes.com/recipes/86/world-cuisine/").text
+soup=BeautifulSoup(link,'html.parser')
+
+data_container=soup.find_all(class_="carouselNav__link recipeCarousel__link",href=True,limit=7)
+
+href=[item['href'] for item in data_container]
+cuisine_name=[(item.find(class_="carouselNav__linkText").get_text()).strip() for item in data_container]
+
+print(href)
+print(cuisine_name)
+dishes_count=[]
+for i in href:
+    linkk=requests.get(i).text
+    soupp=BeautifulSoup(linkk,'html.parser')
+    cuisine_data=soupp.find_all(class_="recipeCard")
+    dishes_count.append(len(cuisine_data))
+
+print(dishes_count)
--- extract&represent.py
+++ extract&represent.py
@@ -1,56 +1,60 @@
-# import requests
-# from bs4 import BeautifulSoup
+import requests
+from bs4 import BeautifulSoup
 
-# link=requests.get("https://www.allrecipes.com/").text
-# soup=BeautifulSoup(link,'html.parser')
-# data_container=soup.find_all(class_='recipeCard__imageLink',href=True,limit=6)
-# href=[item['href'] for item in data_container]
-# Recipi_id,ingrediant_section,cooking_instruction=[],[],[]
-# for url in href:
-#     linkk=requests.get(url).text
-#     soupp=BeautifulSoup(linkk,'html5lib')
-#     data=soupp.find(class_="recipe-summary")
-#     data2=soupp.find(id="lst_ingredients_1")
-#     data3=soupp.find(id="lst_ingredients_2")
-#     recipe_directions =soupp.find(class_="directions--section") 
 
-#     Recipe_ID=data.find('h1',class_="recipe-summary__h1") 
-#     if(Recipe_ID):
-#         Recipi_id.append(Recipe_ID.get_text())
-#         author=data2.find_all(class_="recipe-ingred_txt")
-#         author2=data3.find_all(class_="recipe-ingred_txt")
-#         ind=[]
-#         for i in author:
-#             ind.append(i.get_text())
-#         for i in author2:
-#             ind.append(i.get_text())
+def print_output(ind,dire):
+    print('____'*15+"RECIPE"+'____'*15)
 
-#         ingrediant_section.append(ind)
-#         dire=[]
-#         direction=recipe_directions.find_all(class_="recipe-directions__list--item") 
-#         for i in direction:
-#             dire.append((i.get_text()).strip())
-#         cooking_instruction.append(dire)
+    print()
+    print("INGREDIENT SECTION")
+    print()
+    for ingr in range(len(ind)-1):
+        print(ind[ingr])
 
-#         print('__'*15+"RECIPE"+'__'*15)
+    print()
+    print("COOKING INSTRUCTION")
+    print()
+    for cook in range(len(dire)-1):
+        print(str(cook+1)+'.',dire[cook])
 
-#         print()
-#         print("INGREDIENT SECTION")
-#         print()
-#         for ingr in range(len(ind)-1):
-#             print(ind[ingr])
+link=requests.get("https://www.allrecipes.com/").text
+soup=BeautifulSoup(link,'html.parser')
+data_container=soup.find_all(class_='recipeCard__imageLink',href=True,limit=6)
 
-#         print()
-#         print("COOKING INSTRUCTION")
-#         print()
-#         for cook in range(len(dire)-1):
-#             print(str(cook+1)+'.',dire[cook])
+href=[item['href'] for item in data_container]
 
-# # df=pd.DataFrame(
-# #     {
-# #     'Recipe_ID':Recipi_id,
-# #     'Ingredient_Section':ingrediant_section,
-# #     'Cooking_Instructions':cooking_instruction
-# #     })
-# # df.to_json('recipe.json')
-# # df.to_csv('recipe.csv')
+Recipi_id,ingrediant_section,cooking_instruction=[],[],[]
+
+for url in href:
+    linkk=requests.get(url).text
+    soupp=BeautifulSoup(linkk,'html5lib')
+    lst_ingredients_1=soupp.find(id="lst_ingredients_1")
+    lst_ingredients_2=soupp.find(id="lst_ingredients_2")
+    recipe_directions =soupp.find(class_="directions--section") 
+
+    title=soupp.find('h1',class_="recipe-summary__h1") 
+    if(title):
+        Recipi_id.append(title.get_text())
+        ing_text=lst_ingredients_1.find_all(class_="recipe-ingred_txt")
+        ind=[]
+        for i in ing_text_1:
+            ind.append(i.get_text())
+
+        ingrediant_section.append(ind)
+        dire=[]
+        direction=recipe_directions.find_all(class_="recipe-directions__list--item") 
+        for i in direction:
+            dire.append((i.get_text()).strip())
+        cooking_instruction.append(dire)
+        print_output(ind,dire)
+    else:
+        title2=soupp.find('h1',class_="headline heading-content").get_text()
+        Recipi_id.append(title2)
+        ingrediant_container=soupp.find_all(class_="ingredients-item-name")
+        ingrediant=[ (item.get_text()).strip() for item in ingrediant_container]
+        direction_container=soupp.find_all(class_="paragraph")
+        direct=[(item.find('p').get_text()).strip() for item in direction_container]
+        Recipi_id.extend(title2)
+        ingrediant_section.extend(ingrediant)
+        cooking_instruction.extend(direct)
+        print_output(ingrediant,direct)
